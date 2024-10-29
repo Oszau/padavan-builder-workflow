19ede121651d5af0d310f63bdfe53a104e80a13c
Oszau <oszau@mail.ru>
2024-10-29 17:00:02 +0300

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -1,2 +1,2 @@
-rm padavan-ng/trunk/configs/boards/DLINK/DIR-320B1/partitions.config
-ln -s padavan-ng/trunk/configs/boards/pt_ralink_8m.config padavan-ng/trunk/configs/boards/DLINK/DIR-320B1/partitions.config
+rm padavan-ng/trunk/configs/boards/DLINK/DIR-320B1/partitions.config &&
+cp padavan-ng/trunk/configs/boards/pt_ralink_8m.config padavan-ng/trunk/configs/boards/DLINK/DIR-320B1/partitions.config
