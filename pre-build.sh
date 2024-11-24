d4bc27fb093c0f72502a58af21c60a1717cca9a0
Oszau <oszau@mail.ru>
2024-11-24 10:28:10 +0300

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -1,4 +1,2 @@
 sed -i 's/ISP_INTERFACE=/ISP_INTERFACE=ppp0/' padavan-ng/trunk/user/nfqws/zapret/config
 sed -i 's/TCP_PORTS=80,443/TCP_PORTS=443/' padavan-ng/trunk/user/nfqws/zapret/config
-sed -i 's/--with-nghttp3/--without-nghttp3/' padavan-ng/trunk/libs/libcurl/Makefile
-sed -i 's/--with-ngtcp2/--without-ngtcp2/' padavan-ng/trunk/libs/libcurl/Makefile
