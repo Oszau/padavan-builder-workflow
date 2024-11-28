2154612ab2d5f5e2a14067692e937f2f4d29aee2
Oszau <oszau@mail.ru>
2024-11-28 09:18:16 +0300

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -1,2 +1,3 @@
 sed -i 's/ISP_INTERFACE=/ISP_INTERFACE=ppp0/' padavan-ng/trunk/user/nfqws/zapret/config
 sed -i 's/TCP_PORTS=80,443/TCP_PORTS=443/' padavan-ng/trunk/user/nfqws/zapret/config
+rm $(ROMFSDIR)/etc_ro/ca-certificates.crt
