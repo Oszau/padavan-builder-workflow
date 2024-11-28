2f6204dfec62cf0630b79d3e8a02beaa04e1fc2f
Oszau <oszau@mail.ru>
2024-11-29 01:42:36 +0300

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -3,7 +3,3 @@ sed -i 's/TCP_PORTS=80,443/TCP_PORTS=443/' padavan-ng/trunk/user/nfqws/zapret/co
 
 rm padavan-ng/trunk/configs/boards/$CONFIG_VENDOR/$CONFIG_FIRMWARE_PRODUCT_ID/partitions.config
 cp padavan-ng/trunk/configs/boards/pt_ralink_8m.config padavan-ng/trunk/configs/boards/$CONFIG_VENDOR/$CONFIG_FIRMWARE_PRODUCT_ID/partitions.config
-
-sed -i 's:$(ROMFSINST) /etc_ro/ca-certificates.crt:#$(ROMFSINST) /etc_ro/ca-certificates.crt:' padavan-ng/trunk/libs/libssl-1.1/Makefile
-sed -i 's:$(ROMFSINST) /etc_ro/ca-certificates.crt:#$(ROMFSINST) /etc_ro/ca-certificates.crt:' padavan-ng/trunk/libs/libssl-3.1/Makefile
-sed -i 's:$(ROMFSINST) /etc_ro/ca-certificates.crt:#$(ROMFSINST) /etc_ro/ca-certificates.crt:' padavan-ng/trunk/libs/libssl-3.3/Makefile
