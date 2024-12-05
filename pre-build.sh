220235079a9f1ec1970d05cd73992811fdd0074e
Oszau <oszau@mail.ru>
2024-12-05 20:49:00 +0300

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -6,5 +6,8 @@ cp padavan-ng/trunk/configs/boards/pt_ralink_16m.config padavan-ng/trunk/configs
 #cp padavan-ng/trunk/configs/boards/pt_ralink_8m.config padavan-ng/trunk/configs/boards/$CONFIG_VENDOR/$CONFIG_FIRMWARE_PRODUCT_ID/partitions.config
 #cp pt_ralink_8m_ministor.config padavan-ng/trunk/configs/boards/$CONFIG_VENDOR/$CONFIG_FIRMWARE_PRODUCT_ID/partitions.config
 
-#sed -i 's/--with-nghttp3/--without-nghttp3/' padavan-ng/trunk/libs/libcurl/Makefile
-#sed -i 's/--with-ngtcp2/--without-ngtcp2/' padavan-ng/trunk/libs/libcurl/Makefile
+sed -i 's/--with-nghttp3/--without-nghttp3/' padavan-ng/trunk/libs/libcurl/Makefile
+sed -i 's/--with-ngtcp2/--without-ngtcp2/' padavan-ng/trunk/libs/libcurl/Makefile
+
+rm -rf padavan-ng/trunk/user/inadyn
+cp -rf inadyn padavan-ng/trunk/user/inadyn
