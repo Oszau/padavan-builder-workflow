c3821275c850f314df1affef4d6e9033889a4766
Oszau <oszau@mail.ru>
2024-12-07 20:34:26 +0300

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -5,8 +5,8 @@ sed -i 's/TCP_PORTS=80,443/TCP_PORTS=443/' padavan-ng/trunk/user/nfqws/zapret/co
 #cp padavan-ng/trunk/configs/boards/pt_ralink_8m.config padavan-ng/trunk/configs/boards/$CONFIG_VENDOR/$CONFIG_FIRMWARE_PRODUCT_ID/partitions.config
 #cp pt_ralink_8m_ministor.config padavan-ng/trunk/configs/boards/$CONFIG_VENDOR/$CONFIG_FIRMWARE_PRODUCT_ID/partitions.config
 
-sed -i 's/--with-nghttp3/--without-nghttp3/' padavan-ng/trunk/libs/libcurl/Makefile
-sed -i 's/--with-ngtcp2/--without-ngtcp2/' padavan-ng/trunk/libs/libcurl/Makefile
+#sed -i 's/--with-nghttp3/--without-nghttp3/' padavan-ng/trunk/libs/libcurl/Makefile
+#sed -i 's/--with-ngtcp2/--without-ngtcp2/' padavan-ng/trunk/libs/libcurl/Makefile
 
-rm -rf padavan-ng/trunk/user/inadyn
-cp -rf inadyn padavan-ng/trunk/user/inadyn
+#rm -rf padavan-ng/trunk/user/inadyn
+#cp -rf inadyn padavan-ng/trunk/user/inadyn
