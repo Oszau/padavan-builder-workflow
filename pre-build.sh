dd2fc3570e9b0f4ec6571e3e87f5e8a385f00a2d
Oszau <oszau@mail.ru>
2024-12-30 14:21:05 +0300

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -24,4 +24,5 @@ sed -i '573s/.*/KBUILD_CFLAGS += -Os -fno-reorder-blocks -fno-tree-ch/' padavan-
 #sed -i 's/# CONFIG_NC_EXTRA is not set/CONFIG_NC_EXTRA=y/' padavan-ng/trunk/configs/boards/busybox.config
 #sed -i 's/# CONFIG_NC_110_COMPAT is not set/CONFIG_NC_110_COMPAT=y/' padavan-ng/trunk/configs/boards/busybox.config
 
-sed -i '199a\$(ROMFSINST) -p +x /etc_ro/storage/ASUSddns.sh' padavan-ng/trunk/user/Makefile
+sed -i '198a\$(ROMFSINST) -p +x /etc_ro/ASUSddns.sh' padavan-ng/trunk/user/Makefile
+sed -i '199a\$(ROMFSINST) -s /etc_ro/ASUSddns.sh /etc/storage/ASUSddns.sh' padavan-ng/trunk/user/Makefile
