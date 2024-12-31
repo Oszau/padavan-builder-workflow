5d38e68603a4689037595435f4fb2d4f4cb7678c
Oszau <oszau@mail.ru>
2024-12-31 12:19:17 +0300

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -7,6 +7,8 @@ sed -i 's/TCP_PORTS=80,443/TCP_PORTS=443/' padavan-ng/trunk/user/nfqws/zapret/co
 
 #sed -i 's/--with-nghttp3/--without-nghttp3/' padavan-ng/trunk/libs/libcurl/Makefile
 #sed -i 's/--with-ngtcp2/--without-ngtcp2/' padavan-ng/trunk/libs/libcurl/Makefile
+
+sed -i '6s/.*/CFLAGS  += -Os -ffunction-sections -fdata-sections -fvisibility=hidden -fPIC/' padavan-ng/trunk/user/doh_proxy/Makefile
 sed -i 's:$(ROMFSINST) $(SRC_NAME)/build/https_dns_proxy /usr/sbin/doh_proxy2:$(ROMFSINST) -s /usr/sbin/doh_proxy /usr/sbin/doh_proxy2:' padavan-ng/trunk/user/doh_proxy/Makefile
 
 sed -i '12s/.*/COPTS = $(CPUFLAGS) -Os $(filter-out -O%, $(CFLAGS))/' padavan-ng/trunk/libs/libssl-1.1/Makefile
