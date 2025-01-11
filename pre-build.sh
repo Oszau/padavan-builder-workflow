40aa28746bc1e40080df71eb5c5d760c76016174
Oszau <oszau@mail.ru>
2025-01-11 16:06:37 +0300

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -13,7 +13,7 @@ cp padavan-ng/trunk/configs/boards/pt_ralink_8m.config padavan-ng/trunk/configs/
 #echo "*/5 * * * * /usr/bin/ASUSddns.sh $(nvram get wan_hwaddr) $(nvram get secret_code) $(nvram get rt_ssid) update logger" > /etc/storage/cron/crontabs/admin
 
 cp sysinfo padavan-ng/trunk/user/optware/sysinfo
-sed -i '13a\  $(ROMFSINST) -p +x $(THISDIR)/sysinfo /usr/bin/sysinfo' padavan-ng/trunk/user/optware/Makefile
+sed -i '13a\    $(ROMFSINST) -p +x /usr/bin/sysinfo' padavan-ng/trunk/user/optware/Makefile
 cat padavan-ng/trunk/user/optware/Makefile
 
 #sed -i 's/--with-nghttp3/--without-nghttp3/' padavan-ng/trunk/libs/libcurl/Makefile
