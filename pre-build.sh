457bccede13e25c492ce6c26f4a70983e45af491
Oszau <oszau@mail.ru>
2025-01-13 20:40:04 +0300

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -5,18 +5,19 @@ rm padavan-ng/trunk/configs/boards/$CONFIG_VENDOR/$CONFIG_FIRMWARE_PRODUCT_ID/pa
 cp padavan-ng/trunk/configs/boards/pt_ralink_8m.config padavan-ng/trunk/configs/boards/$CONFIG_VENDOR/$CONFIG_FIRMWARE_PRODUCT_ID/partitions.config
 ##cp pt_ralink_8m_ministor.config padavan-ng/trunk/configs/boards/$CONFIG_VENDOR/$CONFIG_FIRMWARE_PRODUCT_ID/partitions.config
 
+cp clear_RAM.sh padavan-ng/trunk/user/optware/clear_RAM.sh
+sed -i '13a\\t$(ROMFSINST) -p +x /usr/bin/clear_RAM.sh' padavan-ng/trunk/user/optware/Makefile
+
 cp ASUSddns.sh padavan-ng/trunk/user/optware/ASUSddns.sh
-sed -i '13a\\t$(ROMFSINST) -p +x /usr/bin/ASUSddns.sh' padavan-ng/trunk/user/optware/Makefile
-sed -i '14a\\tmkdir -p $(INSTALLDIR)/etc/storage/cron/crontabs' padavan-ng/trunk/user/optware/Makefile
-echo -у "Uncomment for use ASUS ddns service\n#*/5 * * * * /usr/bin/ASUSddns.sh $(nvram get wan_hwaddr) $(nvram get secret_code) $(nvram get rt_ssid) update logger" > /etc/storage/cron/crontabs/admin
+sed -i '14a\\t$(ROMFSINST) -p +x /usr/bin/ASUSddns.sh' padavan-ng/trunk/user/optware/Makefile
+sed -i '15a\\tmkdir -p $(INSTALLDIR)/etc/storage/cron/crontabs' padavan-ng/trunk/user/optware/Makefile
+echo -у "Uncomment for use ASUSddns\n#*/5 * * * * /usr/bin/ASUSddns.sh $(nvram get wan_hwaddr) $(nvram get secret_code) $(nvram get rt_ssid) update logger" > /etc/storage/cron/crontabs/admin
 
 #cp sysinfo padavan-ng/trunk/user/optware/sysinfo
 #ls padavan-ng/trunk/user/optware
 #sed -i '13a\\t$(ROMFSINST) -p +x /usr/bin/sysinfo' padavan-ng/trunk/user/optware/Makefile
 #cat padavan-ng/trunk/user/optware/Makefile
 
-cp clear_RAM.sh padavan-ng/trunk/user/optware/clear_RAM.sh
-sed -i '14a\\t$(ROMFSINST) -p +x /usr/bin/clear_RAM.sh' padavan-ng/trunk/user/optware/Makefile
 #cp get_real_ip.sh padavan-ng/trunk/user/optware/get_real_ip.sh
 #sed -i '15a\\t$(ROMFSINST) -p +x /usr/bin/get_real_ip.sh' padavan-ng/trunk/user/optware/Makefile
 
