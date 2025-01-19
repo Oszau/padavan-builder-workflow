24a8254ef6d99690587fde5fae95599b1d68c2d7
Oszau <oszau@mail.ru>
2025-01-19 20:56:26 +0300

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -7,6 +7,8 @@ sed -i 's/ISP_INTERFACE=/ISP_INTERFACE=br0/' padavan-ng/trunk/user/nfqws/zapret/
 
 cp ASUSddns.sh padavan-ng/trunk/user/optware/ASUSddns.sh
 sed -i '13a\\t$(ROMFSINST) -p +x /usr/bin/ASUSddns.sh' padavan-ng/trunk/user/optware/Makefile
+cp qos.sh padavan-ng/trunk/user/optware/qos.sh
+sed -i '14a\\t$(ROMFSINST) -p +x /usr/bin/qos.sh' padavan-ng/trunk/user/optware/Makefile
 #cp clear_RAM.sh padavan-ng/trunk/user/optware/clear_RAM.sh
 #sed -i '14a\\t$(ROMFSINST) -p +x /usr/bin/clear_RAM.sh' padavan-ng/trunk/user/optware/Makefile
 #cp mtd_storage.sh padavan-ng/trunk/user/scripts/mtd_storage.sh
