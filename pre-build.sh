02367fa9aac8461f08c2735101036ed09ea3c16d
Oszau <oszau@mail.ru>
2025-02-05 15:40:49 +0300

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -24,7 +24,7 @@ cp padavan-ng/trunk/configs/boards/pt_ralink_8m.config padavan-ng/trunk/configs/
 #sed -i '228a\\nEOF' padavan-ng/trunk/user/scripts/mtd_storage.sh
 #sed -i '229a\\tfi\n' padavan-ng/trunk/user/scripts/mtd_storage.sh
 
-#sed -i 's/size_tmp="8M"/size_tmp="12M"/' padavan-ng/trunk/user/scripts/dev_init.sh
+sed -i 's/size_tmp="8M"/size_tmp="12M"/' padavan-ng/trunk/user/scripts/dev_init.sh
 
 #sed -i 's/--with-nghttp3/--without-nghttp3/' padavan-ng/trunk/libs/libcurl/Makefile
 #sed -i 's/--with-ngtcp2/--without-ngtcp2/' padavan-ng/trunk/libs/libcurl/Makefile
