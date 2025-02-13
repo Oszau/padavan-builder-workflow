ce2b3ef97b8cfa21ba6146c607e647587cd17de8
Oszau <oszau@mail.ru>
2025-02-13 10:13:53 +0300

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -42,11 +42,11 @@ sed -i '573s/.*/KBUILD_CFLAGS += -Os -fno-reorder-blocks -fno-tree-ch/' padavan-
 
 gzip -kc padavan-ng/trunk/libs/libssl-1.1/ca-certificates.crt > padavan-ng/trunk/libs/libssl-1.1/ca-certificates.crt.gz
 ls -la padavan-ng/trunk/libs/libssl-1.1
-sed -i 's\$(ROMFSINST) /etc_ro/ca-certificates.crt\$(ROMFSINST) /etc_ro/ca-certificates.crt.gz\' padavan-ng/trunk/libs/libssl-1.1/Makefile
+sed -i 's:$(ROMFSINST) /etc_ro/ca-certificates.crt:$(ROMFSINST) /etc_ro/ca-certificates.crt.gz:' padavan-ng/trunk/libs/libssl-1.1/Makefile
 cat padavan-ng/trunk/libs/libssl-1.1/Makefile
-sed -i 's\$(ROMFSINST) /etc_ro/ca-certificates.crt\#$(ROMFSINST) /etc_ro/ca-certificates.crt\' padavan-ng/trunk/libs/libssl-3.1/Makefile
+#sed -i 's:$(ROMFSINST) /etc_ro/ca-certificates.crt:#$(ROMFSINST) /etc_ro/ca-certificates.crt:' padavan-ng/trunk/libs/libssl-3.1/Makefile
 cat padavan-ng/trunk/libs/libssl-3.1/Makefile
-sed -i 's\$(ROMFSINST) /etc_ro/ca-certificates.crt\#$(ROMFSINST) /etc_ro/ca-certificates.crt\' padavan-ng/trunk/libs/libssl-3.3/Makefile
+#sed -i 's:$(ROMFSINST) /etc_ro/ca-certificates.crt:#$(ROMFSINST) /etc_ro/ca-certificates.crt:' padavan-ng/trunk/libs/libssl-3.3/Makefile
 cat padavan-ng/trunk/libs/libssl-3.3/Makefile
 cp -f dev_init.sh padavan-ng/trunk/user/scripts/dev_init.sh
 cat padavan-ng/trunk/user/scripts/dev_init.sh
