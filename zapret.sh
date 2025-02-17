5e38c179a9f56a49e038920102770de5dced2491
Oszau <oszau@mail.ru>
2025-02-17 15:18:01 +0300

    Update zapret.sh

--- zapret.sh
+++ zapret.sh
@@ -26,7 +26,7 @@ HOSTLIST_DOMAINS="https://github.com/1andrevich/Re-filter-lists/releases/latest/
 HOSTLIST_MARKER="<HOSTLIST>"
 HOSTLIST_NOAUTO_MARKER="<HOSTLIST_NOAUTO>"
 
-[ ! -f "/tmp/auto.list" ] && cp ${ETC_DIR}/zapret/auto.list /tmp/auto.list
+[ ! -f "/tmp/auto.list" ] && cp ${CONFDIR}/auto.list /tmp/auto.list
 
 HOSTLIST_NOAUTO="
   --hostlist=${ETC_DIR}/zapret/user.list
