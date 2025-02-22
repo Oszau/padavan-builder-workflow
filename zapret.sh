7551c84cc07c0b35658f0613800be3553ee34610
Oszau <oszau@mail.ru>
2025-02-22 14:43:15 +0300

    Update zapret.sh

--- zapret.sh
+++ zapret.sh
@@ -28,14 +28,14 @@ HOSTLIST_NOAUTO_MARKER="<HOSTLIST_NOAUTO>"
 
 HOSTLIST_NOAUTO="
   --hostlist=${ETC_DIR}/zapret/user.list
-  --hostlist-auto=/tmp/auto.list
+  --hostlist=${ETC_DIR}/zapret/auto.list
   --hostlist-exclude=${ETC_DIR}/zapret/exclude.list
   --hostlist=/tmp/filter.list
 "
 HOSTLIST="
   --hostlist=${ETC_DIR}/zapret/user.list
   --hostlist-exclude=${ETC_DIR}/zapret/exclude.list
-  --hostlist-auto=/tmp/auto.list
+  --hostlist-auto=/tmp/cache.list
   --hostlist=/tmp/filter.list
 "
 
