a4170381128d295c49f398ad1c2b900996250b1f
Oszau <oszau@mail.ru>
2025-02-26 16:15:48 +0300

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -1,6 +1,6 @@
 sed -i 's/ISP_INTERFACE=/ISP_INTERFACE=br0/' padavan-ng/trunk/user/nfqws/zapret/config
 #sed -i 's/TCP_PORTS=80,443/TCP_PORTS=443/' padavan-ng/trunk/user/nfqws/zapret/config
-cp -f exclude.list padavan-ng/trunk/user/nfqws/zapret/exclude.list
+#cp -f exclude.list padavan-ng/trunk/user/nfqws/zapret/exclude.list
 #cp -f strategy padavan-ng/trunk/user/nfqws/zapret/strategy
 cp -f zapret.sh padavan-ng/trunk/user/nfqws/zapret.sh
 
@@ -8,10 +8,10 @@ cp -f zapret.sh padavan-ng/trunk/user/nfqws/zapret.sh
 #cp padavan-ng/trunk/configs/boards/pt_ralink_8m.config padavan-ng/trunk/configs/boards/$CONFIG_VENDOR/$CONFIG_FIRMWARE_PRODUCT_ID/partitions.config
 #cp pt_ralink_8m_ministor.config padavan-ng/trunk/configs/boards/$CONFIG_VENDOR/$CONFIG_FIRMWARE_PRODUCT_ID/partitions.config
 
-#cp ASUSddns.sh padavan-ng/trunk/user/optware/ASUSddns.sh
-#sed -i '13a\\t$(ROMFSINST) -p +x /usr/bin/ASUSddns.sh' padavan-ng/trunk/user/optware/Makefile
-#cp clear_RAM.sh padavan-ng/trunk/user/optware/clear_RAM.sh
-#sed -i '14a\\t$(ROMFSINST) -p +x /usr/bin/clear_RAM.sh' padavan-ng/trunk/user/optware/Makefile
+cp ASUSddns.sh padavan-ng/trunk/user/optware/ASUSddns.sh
+sed -i '13a\\t$(ROMFSINST) -p +x /usr/bin/ASUSddns.sh' padavan-ng/trunk/user/optware/Makefile
+cp clear_RAM.sh padavan-ng/trunk/user/optware/clear_RAM.sh
+sed -i '14a\\t$(ROMFSINST) -p +x /usr/bin/clear_RAM.sh' padavan-ng/trunk/user/optware/Makefile
 #cp mtd_storage.sh padavan-ng/trunk/user/scripts/mtd_storage.sh
 
 #sed -i 's/size_tmp="8M"/size_tmp="12M"/' padavan-ng/trunk/user/scripts/dev_init.sh
