766c92af1a891e0037fe888acf8be0c6dc0744dc
Oszau <oszau@mail.ru>
2025-03-10 12:09:21 +0300

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -14,8 +14,8 @@ sed -i '13a\\t$(ROMFSINST) -p +x /usr/bin/ASUSddns.sh' padavan-ng/trunk/user/opt
 cp clear_RAM.sh padavan-ng/trunk/user/optware/clear_RAM.sh
 sed -i '14a\\t$(ROMFSINST) -p +x /usr/bin/clear_RAM.sh' padavan-ng/trunk/user/optware/Makefile
 #cp qos.sh padavan-ng/trunk/user/optware/qos.sh
-#sed -i '14a\\t$(ROMFSINST) -p +x /usr/bin/qos.sh' padavan-ng/trunk/user/optware/Makefile
-#cp mtd_storage.sh padavan-ng/trunk/user/scripts/mtd_storage.sh
+sed -i '15a\\t$(ROMFSINST) -p +x /usr/bin/qos.sh' padavan-ng/trunk/user/optware/Makefile
+cp mtd_storage.sh padavan-ng/trunk/user/scripts/mtd_storage.sh
 
 #sed -i '222a\\tif [ ! -f "$dir_crond/$USER" ]; then' padavan-ng/trunk/user/scripts/mtd_storage.sh
 #sed -i '223a\\t\tcat > "$USER" <<EOF' padavan-ng/trunk/user/scripts/mtd_storage.sh
