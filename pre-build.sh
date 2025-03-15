201537fa77fd97732bc41e3e4ec9134a22f7daea
Oszau <oszau@mail.ru>
2025-03-15 21:37:35 +0300

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -14,14 +14,20 @@ cp clear_RAM.sh padavan-ng/trunk/user/optware/clear_RAM.sh
 sed -i '14a\\t$(ROMFSINST) -p +x /usr/bin/clear_RAM.sh' padavan-ng/trunk/user/optware/Makefile
 #cp mtd_storage.sh padavan-ng/trunk/user/scripts/mtd_storage.sh
 
+cp -f Makefile_nfqws padavan-ng/trunk/user/nfqws/Makefile
+
 #sed -i 's/size_tmp="8M"/size_tmp="12M"/' padavan-ng/trunk/user/scripts/dev_init.sh
 
 #sed -i 's/--with-nghttp3/--without-nghttp3/' padavan-ng/trunk/libs/libcurl/Makefile
 #sed -i 's/--with-ngtcp2/--without-ngtcp2/' padavan-ng/trunk/libs/libcurl/Makefile
 #sed -i 's:$(ROMFSINST) $(SRC_NAME)/build/https_dns_proxy /usr/sbin/doh_proxy2:$(ROMFSINST) -s /usr/sbin/doh_proxy /usr/sbin/doh_proxy2:' padavan-ng/trunk/user/doh_proxy/Makefile
-cp -f Makefile_nghttp3 padavan-ng/trunk/libs/libnghttp3/Makefile
-cp -f disabled-examples-in-cmakelists.patch padavan-ng/trunk/libs/libngtcp2/disabled-examples-in-cmakelists.patch
-cp -f Makefile_ngtcp2 padavan-ng/trunk/libs/libngtcp2/Makefile
+#cp -f Makefile_nghttp3 padavan-ng/trunk/libs/libnghttp3/Makefile
+#cp -f disabled-examples-in-cmakelists.patch padavan-ng/trunk/libs/libngtcp2/disabled-examples-in-cmakelists.patch
+#cp -f Makefile_ngtcp2 padavan-ng/trunk/libs/libngtcp2/Makefile
+
+cp -f defaults.c padavan-ng/trunk/user/shared/defaults.c
+cp -f defaults.h padavan-ng/trunk/user/shared/defaults.h
+cp -f firewall_ex.c padavan-ng/trunk/user/rc/firewall_ex.c
 
 sed -i '12s/.*/COPTS = $(CPUFLAGS) -Os $(filter-out -O%, $(CFLAGS))/' padavan-ng/trunk/libs/libssl-1.1/Makefile
 sed -i '12s/.*/COPTS = $(CPUFLAGS) -Os $(filter-out -O%, $(CFLAGS))/' padavan-ng/trunk/libs/libssl-3.1/Makefile
