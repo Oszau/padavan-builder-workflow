2ef0fc49d33a3141e3bfd791253e015a10668caa
Oszau <oszau@mail.ru>
2025-03-26 21:29:21 +0300

    Update defaults.c

--- defaults.c
+++ defaults.c
@@ -70,8 +70,8 @@ struct nvram_pair router_defaults[] = {
 	{ "wan_netmask", "0.0.0.0" },		/* WAN netmask */
 	{ "wan_gateway", "0.0.0.0" },		/* WAN gateway */
 	{ "wan_dnsenable_x", "1" },
-	{ "wan_dns1_x", "" },
-	{ "wan_dns2_x", "" },
+	{ "wan_dns1_x", "77.88.8.7" },
+	{ "wan_dns2_x", "77.88.8.3" },
 	{ "wan_dns3_x", "" },
 	{ "wan_hostname", "" },			/* WAN hostname */
 	{ "wan_vci", "" },			/* WAN vendor class identifier (OPT-60) */
