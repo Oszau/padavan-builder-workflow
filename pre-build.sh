5422f5a0598763247ba8bcaeb71b4197dd5890c0
Oszau <oszau@mail.ru>
2025-04-14 08:30:09 +0300

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -4,7 +4,7 @@ sed -i 's/ISP_INTERFACE=/ISP_INTERFACE=br0/' padavan-ng/trunk/user/nfqws/config/
 #cp -f exclude.list padavan-ng/trunk/user/nfqws/zapret/exclude.list
 #cp -f strategy padavan-ng/trunk/user/nfqws/zapret/strategy
 cp -f zapret.sh padavan-ng/trunk/user/nfqws/zapret.sh
-sed -i 's/SRC_VER = 70.5/SRC_VER = 70.6/' padavan-ng/trunk/user/nfqws/Makefile
+#sed -i 's/SRC_VER = 70.5/SRC_VER = 70.6/' padavan-ng/trunk/user/nfqws/Makefile
 #cp -f web_ex.c padavan-ng/trunk/user/httpd/web_ex.c
 #cp -f net_wan.c padavan-ng/trunk/user/rc/net_wan.c
 sed -i '11a\    export TEMP=/tmp' padavan-ng/trunk/user/scripts/profile
