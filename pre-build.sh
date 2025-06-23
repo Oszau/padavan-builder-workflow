b43b055356b5788d78f48bebfdf50524cb64dea0
Oszau <oszau@mail.ru>
2025-06-23 12:23:29 +0300

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -1,1 +1,3 @@
+sed -i 's/ISP_INTERFACE=/ISP_INTERFACE=br0/' padavan-ng/trunk/user/nfqws/zapretsh-main/zapret/usr/share/zapret/config
+cp -f zapret.sh padavan-ng/trunk/user/nfqws/zapretsh-main/zapret/usr/bin/zapret.sh
 cp -f kernel-3.4.x.config padavan-ng/trunk/configs/boards/$CONFIG_VENDOR/$CONFIG_FIRMWARE_PRODUCT_ID/kernel-3.4.x.config
