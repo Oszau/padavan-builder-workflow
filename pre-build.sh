8a60b80ddcdfc98d035e2ac0b8f58932e013da8e
Oszau <oszau@mail.ru>
2025-08-21 13:45:21 +0300

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -1,5 +1,5 @@
 mkdir padavan-ng/trunk/user/minipro
-#cp -f minipro-0.7.4.tar.gz padavan-ng/trunk/user/minipro/minipro-0.7.4.tar.gz
+cp -f minipro-0.7.4.tar.gz padavan-ng/trunk/user/minipro/minipro-0.7.4.tar.gz
 #tar -C padavan-ng/trunk/user/minipro -xvf padavan-ng/trunk/user/minipro/minipro-0.7.4.tar.gz
 #ls -la padavan-ng/trunk/user/minipro
 #ls -la padavan-ng/trunk/user/minipro/minipro-0.7.4
