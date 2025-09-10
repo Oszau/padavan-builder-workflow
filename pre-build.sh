14f3506f00a019860466c16c8a9d6290243358e9
Oszau <oszau@mail.ru>
2025-09-10 08:35:06 +0300

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -15,7 +15,9 @@
 #cp -f exclude.list padavan-ng/trunk/user/nfqws/zapret/exclude.list
 #cp -f strategy padavan-ng/trunk/user/nfqws/zapret/strategy
 #cp -f zapret.sh padavan-ng/trunk/user/nfqws/zapret.sh
-cp -f zapret.sh padavan-ng/trunk/user/nfqws/zapretsh-main/zapret/usr/bin/zapret.sh
+
+#cp -f zapret.sh padavan-ng/trunk/user/nfqws/zapretsh-main/zapret/usr/bin/zapret.sh
+
 #sed -i '8a\\tCFLAGS += -Os'  padavan-ng/trunk/user/nfqws/Makefile
 #sed -i 's/SRC_VER = 70.5/SRC_VER = 70.6/' padavan-ng/trunk/user/nfqws/Makefile
 #cp -f net_wan.c padavan-ng/trunk/user/rc/net_wan.c
