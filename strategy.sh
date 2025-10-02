b3f2340bbe550ebe5c01f56ffe07b7770e2b455e
Oszau <oszau@mail.ru>
2025-10-02 14:07:09 +0300

    Add files via upload

---
+++ strategy.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+ROOT_DIR=/
+
+if [ -f "/opt/etc/init.d/S51nfqws" ]; then
+    ROOT_DIR=/opt
+    /opt/etc/init.d/S51nfqws stop
+elif [ -f "/etc/init.d/nfqws-keenetic" ]; then
+    /etc/init.d/nfqws-keenetic stop
+else
+    /usr/bin/zapret.sh stop
+fi
+
+mkdir -p "$ROOT_DIR/tmp/nfqws-keenetic/strategy/zapret"
+cd "$ROOT_DIR/tmp/nfqws-keenetic/strategy"
+
+RELEASE_URL=`curl -s https://api.github.com/repos/bol-van/zapret/releases/latest | grep browser_download_url | grep 'embedded.tar.gz' | cut -d '"' -f 4`
+curl -SL# $RELEASE_URL -o zapret.tar.gz
+tar -C zapret -xzf zapret.tar.gz
+cd zapret/*/
+
+./install_bin.sh
+SECURE_DNS=1 FWTYPE=iptables SKIP_TPWS=1 ./blockcheck.sh
+
+rm -rf "$ROOT_DIR/tmp/nfqws-keenetic/strategy"
+echo -e "* NOTE: nfqws-keenetic is stopped. Start it manually if necessary! \n"
