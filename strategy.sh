ed0acf017969c649a4112af99f5db683183875ab
Oszau <oszau@mail.ru>
2025-10-03 22:14:56 +0300

    Update strategy.sh

--- strategy.sh
+++ strategy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-ROOT_DIR=/
+ROOT_DIR=
 
 if [ -f "/opt/etc/init.d/S51nfqws" ]; then
     ROOT_DIR=/opt
@@ -15,12 +15,13 @@ mkdir -p "$ROOT_DIR/tmp/nfqws-keenetic/strategy/zapret"
 cd "$ROOT_DIR/tmp/nfqws-keenetic/strategy"
 
 RELEASE_URL=`curl -s https://api.github.com/repos/bol-van/zapret/releases/latest | grep browser_download_url | grep 'embedded.tar.gz' | cut -d '"' -f 4`
-curl -SL# $RELEASE_URL -o zapret.tar.gz
-tar -C zapret -xzf zapret.tar.gz
+BINARY=zapret.tar.gz
+curl -SL# $RELEASE_URL -o $BINARY
+tar -C zapret -xzf $BINARY && rm $BINARY
 cd zapret/*/
 
 ./install_bin.sh
-SECURE_DNS=1 FWTYPE=iptables SKIP_TPWS=1 ./blockcheck.sh
+SECURE_DNS=0 FWTYPE=iptables SKIP_TPWS=1 ./blockcheck.sh
 
 rm -rf "$ROOT_DIR/tmp/nfqws-keenetic/strategy"
 echo -e "* NOTE: nfqws-keenetic is stopped. Start it manually if necessary! \n"
