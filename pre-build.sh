8070a2e5a1a53483d0667733fc172571f4f85d9f
Oszau <oszau@mail.ru>
2025-11-06 10:57:10 +0300

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -9,8 +9,12 @@ cp -f kernel-3.4.x.config padavan-ng/trunk/configs/boards/$CONFIG_VENDOR/$CONFIG
 #sed -i '14a\\t$(ROMFSINST) -p +x /usr/bin/clear_RAM.sh' padavan-ng/trunk/user/optware/Makefile
 #cp strategy.sh padavan-ng/trunk/user/optware/strategy.sh
 #sed -i '15a\\t$(ROMFSINST) -p +x /usr/bin/strategy.sh' padavan-ng/trunk/user/optware/Makefile
-cp sysinfo padavan-ng/trunk/user/optware/sysinfo
+cp -f sysinfo padavan-ng/trunk/user/optware/sysinfo
 sed -i '13a\\t$(ROMFSINST) -p +x /usr/bin/sysinfo' padavan-ng/trunk/user/optware/Makefile
+
+cp -f mtd_storage_mod.sh padavan-ng/trunk/user/scripts/mtd_storage.sh
+cp -f defaults_mod.c padavan-ng/trunk/user/shared/defaults.c
+cp -f defaults_mod.h padavan-ng/trunk/user/shared/defaults.h
 
 sed -i 's/# CONFIG_FEATURE_CATN is not set/CONFIG_FEATURE_CATN=y/' padavan-ng/trunk/configs/boards/busybox.config
 sed -i 's/# CONFIG_DIFF is not set/CONFIG_DIFF=y/' padavan-ng/trunk/configs/boards/busybox.config
