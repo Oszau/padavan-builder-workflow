71e3ca4c43e7d4297b73fb2a4ded4ba3710da0f8
Oszau <oszau@mail.ru>
2025-11-06 11:03:59 +0300

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -94,5 +94,12 @@ cp -f zapret.sh padavan-ng/trunk/user/nfqws/zapretsh-main/zapret/usr/bin/zapret.
 cp -f pt_ralink_8m_ministor.config padavan-ng/trunk/configs/boards/$CONFIG_VENDOR/$CONFIG_FIRMWARE_PRODUCT_ID/partitions.config
 cp -f kernel-3.4.x.config padavan-ng/trunk/configs/boards/$CONFIG_VENDOR/$CONFIG_FIRMWARE_PRODUCT_ID/kernel-3.4.x.config
 
+cp -f sysinfo padavan-ng/trunk/user/optware/sysinfo
+sed -i '13a\\t$(ROMFSINST) -p +x /usr/bin/sysinfo' padavan-ng/trunk/user/optware/Makefile
+
+cp -f mtd_storage_mod.sh padavan-ng/trunk/user/scripts/mtd_storage.sh
+cp -f defaults_mod.c padavan-ng/trunk/user/shared/defaults.c
+cp -f defaults_mod.h padavan-ng/trunk/user/shared/defaults.h
+
 sed -i 's/# CONFIG_FEATURE_CATN is not set/CONFIG_FEATURE_CATN=y/' padavan-ng/trunk/configs/boards/busybox.config
 sed -i 's/# CONFIG_DIFF is not set/CONFIG_DIFF=y/' padavan-ng/trunk/configs/boards/busybox.config
