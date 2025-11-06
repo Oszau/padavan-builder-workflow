62bddcaa452573e92845c447701866f01edb3c2b
Oszau <oszau@mail.ru>
2025-11-06 09:50:55 +0300

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -3,5 +3,14 @@ cp -f zapret.sh padavan-ng/trunk/user/nfqws/zapretsh-main/zapret/usr/bin/zapret.
 cp -f pt_ralink_8m_ministor.config padavan-ng/trunk/configs/boards/$CONFIG_VENDOR/$CONFIG_FIRMWARE_PRODUCT_ID/partitions.config
 cp -f kernel-3.4.x.config padavan-ng/trunk/configs/boards/$CONFIG_VENDOR/$CONFIG_FIRMWARE_PRODUCT_ID/kernel-3.4.x.config
 
+#cp ASUSddns.sh padavan-ng/trunk/user/optware/ASUSddns.sh
+#sed -i '13a\\t$(ROMFSINST) -p +x /usr/bin/ASUSddns.sh' padavan-ng/trunk/user/optware/Makefile
+#cp clear_RAM.sh padavan-ng/trunk/user/optware/clear_RAM.sh
+#sed -i '14a\\t$(ROMFSINST) -p +x /usr/bin/clear_RAM.sh' padavan-ng/trunk/user/optware/Makefile
+#cp strategy.sh padavan-ng/trunk/user/optware/strategy.sh
+#sed -i '15a\\t$(ROMFSINST) -p +x /usr/bin/strategy.sh' padavan-ng/trunk/user/optware/Makefile
+cp sysinfo padavan-ng/trunk/user/optware/sysinfo
+sed -i '13a\\t$(ROMFSINST) -p +x /usr/bin/sysinfo' padavan-ng/trunk/user/optware/Makefile
+
 sed -i 's/# CONFIG_FEATURE_CATN is not set/CONFIG_FEATURE_CATN=y/' padavan-ng/trunk/configs/boards/busybox.config
 sed -i 's/# CONFIG_DIFF is not set/CONFIG_DIFF=y/' padavan-ng/trunk/configs/boards/busybox.config
