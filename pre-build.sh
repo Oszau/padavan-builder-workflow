ce38124b77039bed6afae154da5aa52922452acd
Oszau <oszau@mail.ru>
2026-01-09 17:29:13 +0300

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -13,11 +13,11 @@ sed -i '13a\\t$(ROMFSINST) -p +x /usr/bin/ASUSddns.sh' padavan-ng/trunk/user/opt
 #cp strategy.sh padavan-ng/trunk/user/optware/strategy.sh
 #sed -i '15a\\t$(ROMFSINST) -p +x /usr/bin/strategy.sh' padavan-ng/trunk/user/optware/Makefile
 cp -f sysinfo padavan-ng/trunk/user/optware/sysinfo
-sed -i '14a\\t$(ROMFSINST) -p +x /usr/bin/sysinfo' padavan-ng/trunk/user/optware/Makefile
+#sed -i '14a\\t$(ROMFSINST) -p +x /usr/bin/sysinfo' padavan-ng/trunk/user/optware/Makefile
 
 cp -f mtd_storage_mod.sh padavan-ng/trunk/user/scripts/mtd_storage.sh
 cp -f defaults_mod.c padavan-ng/trunk/user/shared/defaults.c
 cp -f defaults_mod.h padavan-ng/trunk/user/shared/defaults.h
 
-sed -i 's/# CONFIG_FEATURE_CATN is not set/CONFIG_FEATURE_CATN=y/' padavan-ng/trunk/configs/boards/busybox.config
-sed -i 's/# CONFIG_DIFF is not set/CONFIG_DIFF=y/' padavan-ng/trunk/configs/boards/busybox.config
+#sed -i 's/# CONFIG_FEATURE_CATN is not set/CONFIG_FEATURE_CATN=y/' padavan-ng/trunk/configs/boards/busybox.config
+#sed -i 's/# CONFIG_DIFF is not set/CONFIG_DIFF=y/' padavan-ng/trunk/configs/boards/busybox.config
