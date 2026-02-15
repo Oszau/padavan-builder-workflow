31dc85df8076872af17f98f41a6fffaad0a1d447
Oszau <oszau@mail.ru>
2026-02-15 09:29:01 +0300

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -1,6 +1,6 @@
-#sed -i 's/# CONFIG_FEATURE_CATN is not set/CONFIG_FEATURE_CATN=y/' padavan-ng/trunk/configs/boards/busybox.config
-#sed -i 's/# CONFIG_DIFF is not set/CONFIG_DIFF=y/' padavan-ng/trunk/configs/boards/busybox.config
+sed -i 's/# CONFIG_FEATURE_CATN is not set/CONFIG_FEATURE_CATN=y/' trunk/configs/boards/busybox.config
+sed -i 's/# CONFIG_DIFF is not set/CONFIG_DIFF=y/' trunk/configs/boards/busybox.config
 
 sed -i 's/CONFIG_IPCALC is not set/CONFIG_IPCALC=y/' trunk/configs/boards/busybox.config
-#sed -i 's/CONFIG_FEATURE_IPCALC_LONG_OPTIONS is not set/CONFIG_FEATURE_IPCALC_LONG_OPTIONS=y/' padavan-ng/trunk/configs/boards/busybox.config
-#sed -i 's/CONFIG_FEATURE_IPCALC_FANCY is not set/CONFIG_FEATURE_IPCALC_FANCY=y/' padavan-ng/trunk/configs/boards/busybox.config
+sed -i 's/CONFIG_FEATURE_IPCALC_LONG_OPTIONS is not set/CONFIG_FEATURE_IPCALC_LONG_OPTIONS=y/' trunk/configs/boards/busybox.config
+sed -i 's/CONFIG_FEATURE_IPCALC_FANCY is not set/CONFIG_FEATURE_IPCALC_FANCY=y/' trunk/configs/boards/busybox.config
