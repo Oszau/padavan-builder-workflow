fd48ca0ca5ef5d6b6a6a81a3846f93d9359fdc72
Oszau <oszau@mail.ru>
2026-02-26 15:18:36 +0300

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -1,7 +1,7 @@
 apt update
 apt install -y gcc-multilib
 
-#cp -f pt_ralink_8m_ministor.config padavan-ng/trunk/configs/boards/$CONFIG_VENDOR/$CONFIG_FIRMWARE_PRODUCT_ID/partitions.config
+cp -f pt_ralink_8m_ministor.config padavan-ng/trunk/configs/boards/$CONFIG_VENDOR/$CONFIG_FIRMWARE_PRODUCT_ID/partitions.config
 
 #sed -i 's/# CONFIG_FEATURE_CATN is not set/CONFIG_FEATURE_CATN=y/' padavan-ng/trunk/configs/boards/busybox.config
 #sed -i 's/# CONFIG_DIFF is not set/CONFIG_DIFF=y/' padavan-ng/trunk/configs/boards/busybox.config
