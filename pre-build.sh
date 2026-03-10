b1c5b3df0fb8f6b0755987c62e04a6e22d1b4655
Oszau <oszau@mail.ru>
2026-03-10 11:17:19 +0300

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -1,6 +1,8 @@
 apt update
 apt install -y gcc-multilib
 
+sed -i 's/# CONFIG_BC is not set/CONFIG_BC=y/' padavan-ng/trunk/configs/boards/busybox.config
+
 sed -i 's/# CONFIG_FEATURE_CATN is not set/CONFIG_FEATURE_CATN=y/' padavan-ng/trunk/configs/boards/busybox.config
 sed -i 's/# CONFIG_DIFF is not set/CONFIG_DIFF=y/' padavan-ng/trunk/configs/boards/busybox.config
 
