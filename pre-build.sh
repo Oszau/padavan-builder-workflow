6662a64335d9c19f63ef8c70d0423e5a9f2b5beb
Oszau <oszau@mail.ru>
2026-03-10 12:19:32 +0300

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -10,7 +10,29 @@ sed -i 's/# CONFIG_IPCALC is not set/CONFIG_IPCALC=y/' padavan-ng/trunk/configs/
 sed -i 's/# CONFIG_FEATURE_IPCALC_LONG_OPTIONS is not set/CONFIG_FEATURE_IPCALC_LONG_OPTIONS=y/' padavan-ng/trunk/configs/boards/busybox.config
 sed -i 's/# CONFIG_FEATURE_IPCALC_FANCY is not set/CONFIG_FEATURE_IPCALC_FANCY=y/' padavan-ng/trunk/configs/boards/busybox.config
 
-#kernel
-sed -i '636a\\tCONFIG_NET_SCH_TBF=m -p +x padavan-ng/trunk/configs/boards/$CONFIG_VENDOR/$CONFIG_FIRMWARE_PRODUCT_ID/kernel-3.4.x.config
-sed -i '1334a\\tCONFIG_FSCACHE=m -p +x padavan-ng/trunk/configs/boards/$CONFIG_VENDOR/$CONFIG_FIRMWARE_PRODUCT_ID/kernel-3.4.x.config
-sed -i '1335a\\tCONFIG_FSCACHE_STATS=y -p +x padavan-ng/trunk/configs/boards/$CONFIG_VENDOR/$CONFIG_FIRMWARE_PRODUCT_ID/kernel-3.4.x.config
+#kernerl v2
+sed -i '1335a\\tCONFIG_FSCACHE=m -p +x padavan-ng/trunk/configs/boards/$CONFIG_VENDOR/$CONFIG_FIRMWARE_PRODUCT_ID/kernel-3.4.x.config
+sed -i '1336a\\tCONFIG_FSCACHE_STATS=y -p +x padavan-ng/trunk/configs/boards/$CONFIG_VENDOR/$CONFIG_FIRMWARE_PRODUCT_ID/kernel-3.4.x.config
+sed -i '636a\\tCONFIG_NET_SCHED=y -p +x padavan-ng/trunk/configs/boards/$CONFIG_VENDOR/$CONFIG_FIRMWARE_PRODUCT_ID/kernel-3.4.x.config
+sed -i '637a\\tCONFIG_NET_SCH_CBQ=m -p +x padavan-ng/trunk/configs/boards/$CONFIG_VENDOR/$CONFIG_FIRMWARE_PRODUCT_ID/kernel-3.4.x.config
+sed -i '638a\\tCONFIG_NET_SCH_HTB=m -p +x padavan-ng/trunk/configs/boards/$CONFIG_VENDOR/$CONFIG_FIRMWARE_PRODUCT_ID/kernel-3.4.x.config
+sed -i '639a\\tCONFIG_NET_SCH_CSZ=m -p +x padavan-ng/trunk/configs/boards/$CONFIG_VENDOR/$CONFIG_FIRMWARE_PRODUCT_ID/kernel-3.4.x.config
+sed -i '640a\\tCONFIG_NET_SCH_PRIO=m -p +x padavan-ng/trunk/configs/boards/$CONFIG_VENDOR/$CONFIG_FIRMWARE_PRODUCT_ID/kernel-3.4.x.config
+sed -i '641a\\tCONFIG_NET_SCH_RED=m -p +x padavan-ng/trunk/configs/boards/$CONFIG_VENDOR/$CONFIG_FIRMWARE_PRODUCT_ID/kernel-3.4.x.config
+sed -i '642a\\tCONFIG_NET_SCH_SFQ=m -p +x padavan-ng/trunk/configs/boards/$CONFIG_VENDOR/$CONFIG_FIRMWARE_PRODUCT_ID/kernel-3.4.x.config
+sed -i '643a\\tCONFIG_NET_SCH_TEQL=m -p +x padavan-ng/trunk/configs/boards/$CONFIG_VENDOR/$CONFIG_FIRMWARE_PRODUCT_ID/kernel-3.4.x.config
+sed -i '644a\\tCONFIG_NET_SCH_TBF=m -p +x padavan-ng/trunk/configs/boards/$CONFIG_VENDOR/$CONFIG_FIRMWARE_PRODUCT_ID/kernel-3.4.x.config
+sed -i '645a\\tCONFIG_NET_SCH_GRED=m -p +x padavan-ng/trunk/configs/boards/$CONFIG_VENDOR/$CONFIG_FIRMWARE_PRODUCT_ID/kernel-3.4.x.config
+sed -i '646a\\tCONFIG_NET_SCH_DSMARK=m -p +x padavan-ng/trunk/configs/boards/$CONFIG_VENDOR/$CONFIG_FIRMWARE_PRODUCT_ID/kernel-3.4.x.config
+sed -i '647a\\tCONFIG_NET_SCH_INGRESS=m -p +x padavan-ng/trunk/configs/boards/$CONFIG_VENDOR/$CONFIG_FIRMWARE_PRODUCT_ID/kernel-3.4.x.config
+sed -i '648a\\tCONFIG_NET_QOS=y -p +x padavan-ng/trunk/configs/boards/$CONFIG_VENDOR/$CONFIG_FIRMWARE_PRODUCT_ID/kernel-3.4.x.config
+sed -i '649a\\tCONFIG_NET_ESTIMATOR=y -p +x padavan-ng/trunk/configs/boards/$CONFIG_VENDOR/$CONFIG_FIRMWARE_PRODUCT_ID/kernel-3.4.x.config
+sed -i '650a\\tCONFIG_NET_CLS=y -p +x padavan-ng/trunk/configs/boards/$CONFIG_VENDOR/$CONFIG_FIRMWARE_PRODUCT_ID/kernel-3.4.x.config
+sed -i '651a\\tCONFIG_NET_CLS_TCINDEX=m -p +x padavan-ng/trunk/configs/boards/$CONFIG_VENDOR/$CONFIG_FIRMWARE_PRODUCT_ID/kernel-3.4.x.config
+sed -i '652a\\tCONFIG_NET_CLS_ROUTE4=m -p +x padavan-ng/trunk/configs/boards/$CONFIG_VENDOR/$CONFIG_FIRMWARE_PRODUCT_ID/kernel-3.4.x.config
+sed -i '653a\\tCONFIG_NET_CLS_ROUTE=y -p +x padavan-ng/trunk/configs/boards/$CONFIG_VENDOR/$CONFIG_FIRMWARE_PRODUCT_ID/kernel-3.4.x.config
+sed -i '654a\\tCONFIG_NET_CLS_FW=m -p +x padavan-ng/trunk/configs/boards/$CONFIG_VENDOR/$CONFIG_FIRMWARE_PRODUCT_ID/kernel-3.4.x.config
+sed -i '655a\\tCONFIG_NET_CLS_U32=m -p +x padavan-ng/trunk/configs/boards/$CONFIG_VENDOR/$CONFIG_FIRMWARE_PRODUCT_ID/kernel-3.4.x.config
+sed -i '656a\\tCONFIG_NET_CLS_RSVP=m -p +x padavan-ng/trunk/configs/boards/$CONFIG_VENDOR/$CONFIG_FIRMWARE_PRODUCT_ID/kernel-3.4.x.config
+sed -i '657a\\tCONFIG_NET_CLS_RSVP6=m -p +x padavan-ng/trunk/configs/boards/$CONFIG_VENDOR/$CONFIG_FIRMWARE_PRODUCT_ID/kernel-3.4.x.config
+sed -i '658a\\tCONFIG_NET_CLS_POLICE=y -p +x padavan-ng/trunk/configs/boards/$CONFIG_VENDOR/$CONFIG_FIRMWARE_PRODUCT_ID/kernel-3.4.x.config
