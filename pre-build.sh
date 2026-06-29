8a046c65090d08b1c1f1117055d16d7a8123da47
Oszau <oszau@mail.ru>
2026-06-29 08:57:11 +0300

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -1,9 +1,10 @@
 rm -r  padavan-ng/trunk/user/dnsmasq
 #ls padavan-ng/trunk/user/
-mkdir padavan-ng/trunk/user/dnsmasq
+#mkdir padavan-ng/trunk/user/dnsmasq
 wget https://github.com/nilabsent/padavan-ng/tree/7438a540504f4bad3417eb0e8b2cda9c9583cb5a/trunk/user/dnsmasq -P padavan-ng/trunk/user/
 ls -la padavan-ng/trunk/user/
 ls -la padavan-ng/trunk/user/dnsmasq/
+break
 
 cp -f zapret.sh padavan-ng/trunk/user/nfqws/zapretsh-main/zapret/usr/bin/zapret.sh
 
